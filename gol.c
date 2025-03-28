f4ec38d50b41e4002f152bb25e146d7d863d5109
rsvn <rizwanshaz2004@gmail.com>
2025-03-29 00:09:36 +0530

    drawing_cell

--- gol.c
+++ gol.c
@@ -2,26 +2,30 @@
 #include<SDL2/SDL.h>
 
 Uint32 WHITE_COLOUR=0xffffffff;
+Uint32 GREY_COLOUR=0x2f2f2f2f;
 int SURFACE_WIDTH=900;
 int SURFACE_HEIGHT=600;
 int CELL_WIDTH=25;
-int LINE_WIDTH=2;
+int LINE_WIDTH=1;
 
 int draw_grid(SDL_Surface* surface,int n_rows,int n_columns){
     for (int i=0; i<n_rows; i++){
     SDL_Rect line=(SDL_Rect) {0,i*CELL_WIDTH,SURFACE_WIDTH,LINE_WIDTH};
-    SDL_FillRect(surface,&line,WHITE_COLOUR);
+    SDL_FillRect(surface,&line,GREY_COLOUR);
            }
     for (int i=0; i<n_columns; i++){
     SDL_Rect line=(SDL_Rect) {i*CELL_WIDTH,0,LINE_WIDTH,SURFACE_HEIGHT};
-    SDL_FillRect(surface,&line,WHITE_COLOUR);
+    SDL_FillRect(surface,&line,GREY_COLOUR);
            }
 }
 
-
-
-
-
+int draw_cell(SDL_Surface* surface,int cell_x,int cell_y){
+    int pxl_x=cell_x*CELL_WIDTH;
+    int pxl_y=cell_y*CELL_WIDTH;
+    
+    SDL_Rect cell =(SDL_Rect) {pxl_x,pxl_y,CELL_WIDTH,CELL_WIDTH};
+    SDL_FillRect(surface,&cell,WHITE_COLOUR);
+}
 
 
 
@@ -37,12 +41,14 @@ int main(){
     SDL_Window* window = SDL_CreateWindow(title,SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,SURFACE_WIDTH,SURFACE_HEIGHT,0);
     SDL_Surface* surface= SDL_GetWindowSurface(window);
 
-    SDL_Rect rect =(SDL_Rect) {30,30,30,30};
-    SDL_FillRect(surface,&rect,WHITE_COLOUR);
 
 
+    int cell_x=5;
+    int cell_y=5;
+    draw_cell(surface,cell_x,cell_y);
+    draw_cell(surface,1,1);
+    draw_cell(surface,0,0);
     draw_grid(surface,n_rows,n_columns);
-
     SDL_UpdateWindowSurface(window);
     SDL_Delay(5000);
 }
