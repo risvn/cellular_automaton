7eef22ff9d8a17d20711369fe214f7cdb1a27e0e
rsvn <rizwanshaz2004@gmail.com>
2025-03-30 03:46:47 +0530

    drawing_matrix and cell_value

--- gol.c
+++ gol.c
@@ -2,11 +2,21 @@
 #include<SDL2/SDL.h>
 
 Uint32 WHITE_COLOUR=0xffffffff;
+Uint32 BLACK_COLOUR=0x00000000;
 Uint32 GREY_COLOUR=0x2f2f2f2f;
 int SURFACE_WIDTH=900;
 int SURFACE_HEIGHT=600;
 int CELL_WIDTH=25;
 int LINE_WIDTH=1;
+
+int draw_cell(SDL_Surface* surface,int cell_x,int cell_y,int cell_value){
+    int pxl_x=cell_x*CELL_WIDTH;
+    int pxl_y=cell_y*CELL_WIDTH;
+    Uint32 colour=cell_value==0?BLACK_COLOUR:WHITE_COLOUR;
+    
+    SDL_Rect cell =(SDL_Rect) {pxl_x,pxl_y,CELL_WIDTH,CELL_WIDTH};
+    SDL_FillRect(surface,&cell,colour);
+}
 
 int draw_grid(SDL_Surface* surface,int n_rows,int n_columns){
     for (int i=0; i<n_rows; i++){
@@ -19,13 +29,32 @@ int draw_grid(SDL_Surface* surface,int n_rows,int n_columns){
            }
 }
 
-int draw_cell(SDL_Surface* surface,int cell_x,int cell_y){
-    int pxl_x=cell_x*CELL_WIDTH;
-    int pxl_y=cell_y*CELL_WIDTH;
-    
-    SDL_Rect cell =(SDL_Rect) {pxl_x,pxl_y,CELL_WIDTH,CELL_WIDTH};
-    SDL_FillRect(surface,&cell,WHITE_COLOUR);
+void draw_matrix(SDL_Surface* surface,int n_rows,int n_columns,int* game_matrix){
+
+    for(int i=0;i<n_rows;i++){
+
+        for(int j=0;j<n_columns;j++){
+           int cell_value=*(game_matrix+ i*n_columns +j);
+            draw_cell(surface,j,i,cell_value);
+          }
+
+      }
+
 }
+
+void init_game_matrix(int n_rows,int n_columns,int* game_matrix){
+    for(int i=0;i<n_rows;i++){
+
+        for(int j=0;j<n_columns;j++){
+           *(game_matrix+i*n_columns+j)= rand()%2;  
+
+          }
+
+      }
+ }
+
+
+
 
 
 
@@ -43,11 +72,17 @@ int main(){
 
 
 
-    int cell_x=5;
-    int cell_y=5;
-    draw_cell(surface,cell_x,cell_y);
-    draw_cell(surface,1,1);
-    draw_cell(surface,0,0);
+
+
+
+
+
+
+    int game_matrix [n_rows][n_columns];
+    init_game_matrix(n_rows,n_columns,game_matrix[0]);
+    draw_matrix(surface,n_rows,n_columns,game_matrix[0]);
+
+
     draw_grid(surface,n_rows,n_columns);
     SDL_UpdateWindowSurface(window);
     SDL_Delay(5000);
